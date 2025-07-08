0f7767bf55c653cb2c6e8afd9b05b293078fa8aa
woshiren <yejiacong@outlook.com>
2025-07-09 00:44:54 +0800

    修复了time_diff写成相反数的bug

--- query.py
+++ query.py
@@ -102,11 +102,11 @@ def do_query(query_str: str, q_passphrase: str, q_cookies: dict):
 
     print(f"successfully saved to {cs.filename}")
     exhaust_time = plot.plot(cs)
-    time_diff = datetime.now() - exhaust_time
+    time_diff = exhaust_time - datetime.now()
     print(f"exhaust time: {exhaust_time} ({time_diff.days} days, {time_diff.seconds // 3600} hours, {(time_diff.seconds // 60) % 60} minutes)")
     print(f"total seconds: {time_diff.total_seconds()}")
 
-    if ((remain < 5 and mail_config["mail_notify"]) or mail_config["force_notify"]):
+    if ((time_diff.days < 1 and mail_config["mail_notify"]) or mail_config["force_notify"]):
         mail_config["receivers"] = receiver_dict.get(room_name, [mail_config["sender"]])
         ret = notify.mail_notification(
             mail_config=mail_config,
